3e42a45b07c9decb059f32c873641dd3e5cd7523
Siddharta-paul <siddhartapaul01@gmail.com>
2021-07-15 13:38:58 +0530

    Initial Commit(A)

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,20 @@
+
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static void openCalculator() {
+		
+		DesiredCapabilities cap = new DesiredCapabilities();
+		
+		
+		
+		
+		
+	}
+}
